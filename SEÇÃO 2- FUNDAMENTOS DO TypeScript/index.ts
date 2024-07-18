a23a998ec6f1c8eff354fd835b78e16822625f5f
Claudio Matheus <claudiomatheus055@gmail.com>
2024-07-18 15:37:04 -0300

    Atualizando as pastas

--- SEÇÃO 2- FUNDAMENTOS DO TypeScript/index.ts
+++ SEÇÃO 2- FUNDAMENTOS DO TypeScript/index.ts
@@ -1,91 +1,47 @@
-// 1 - ARRAYS
+// 1 - TIPO NUMBER
 
-let numbers: number[] = [1, 2, 3];
+let x: number = 10;
 
-numbers.push(5);
+console.log(typeof x);
 
-console.log(numbers[2]);
+const y: number = 15.584848;
 
-// numbers = "teste" // Comentado porque não é permitido atribuir string a um array de números
+console.log(typeof y);
 
-const nome: string[] = ["Matheus", "João"];
+console.log(y);
 
-// nome.push(4) // Comentado porque não é permitido adicionar um número a um array de strings
+console.log(y.toPrecision(3));
 
-// 2 - OUTRA SINTAXE ARRAY
+// 2 - TIPO STRING
 
-const nuns: Array<number> = [100, 200];
+const firstName: string = "Matheus";
 
-nuns.push(300);
+console.log(firstName.toUpperCase());
 
-console.log(nuns);
+let fullName: string;
 
-// nuns.push("Teste") // Comentado porque não é permitido adicionar string a um array de números
+const lastName: string = "Silva";
 
-console.log(nuns[0]);
+fullName = firstName + " " + lastName;
 
-// 3 - TIPO ANY
+console.log(typeof fullName);
 
-const arr1: any[] = [1, "teste", true, [], { nome: "Matheus" }];
+// 3 - TIPO BOOLEAN
 
-console.log(arr1);
+let a: boolean = false;
 
-arr1.push([1, 2, 3]);
+console.log(a);
+console.log(typeof a);
 
-console.log(arr1);
+a = true;
 
-// 4 - PARAMETROS TIPADOS
+console.log(a);
 
-function soma(a: number, b: number): void {
-    console.log(a + b);
-}
+// 4 - INFERENCE E ANNOTATION
 
-soma(4, 5);
+let ann: string = "Teste";
 
-// soma("a", "b") // Comentado porque não é permitido passar strings para uma função que espera números
+let inf = "Teste";
 
-// 5 - RETORNO DE FUNÇÃO
-
-function greeting(name: string): string {
-    // return 5; // Comentado porque não é permitido retornar um número quando a função espera uma string
-    return `Olá ${name}`;
-}
-
-console.log(greeting("Matheus"));
-
-// 6 - FUNÇÃO ANONIMA
-
-setTimeout(function () {
-    const salary: number = 1000;
-
-    console.log(parseFloat(salary.toString()));
-
-    console.log(salary);
-
-}, 2000);
-
-// 7 - TIPOS DE OBJETO
-
-function passCoordinates(coord: { x: number, y: number }) {
-    console.log("X coordinates " + coord.x);
-    console.log("Y coordinates " + coord.y);
-}
-
-const objCoord = { x: 329, y: 84.2 };
-
-passCoordinates(objCoord);
-
-const PessoaObj: { nome: string, surname: string } = { nome: "Matheus", surname: "Ferreira" };
-
-console.log(PessoaObj);
-
-
-
-
-
-
-
-
-
-
-
+// ann = 1
+// inf = 1
